20033ed7e7af4a935c0a00bb715775d4420d2cff
Charalampos Papadopoulos <harris@easy.box>
2024-12-16 17:32:07 +0200

    db configuration and sql functions

---
+++ db/db.js
@@ -0,0 +1,29 @@
+const mysql = require("mysql");
+const config = require("./config");
+const pool = mysql.createPool(config);
+
+const config ={
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+};
+
+const checkRecordExists = (tableName, column, value) => {
+    return new Promise((resolve, reject) => {
+      const query = `SELECT * FROM ${tableName} WHERE ${column} = ? `;
+  
+      pool.query(query, [value], (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results.length ? results[0] : null);
+        }
+      });
+    });
+  };
+
+module.exports ={
+  config,
+  checkRecordExists,
+};
