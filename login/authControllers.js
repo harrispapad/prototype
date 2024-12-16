4f92495de068f01822860f83b26c3e74e14dc1ea
Charalampos Papadopoulos <harris@easy.box>
2024-12-16 17:37:01 +0200

    login authorization functions

---
+++ login/authControllers.js
@@ -0,0 +1,56 @@
+const { v4: uuidv4 } = require("uuid");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { checkRecordExists } = require("../db/db");
+const mysql = require("mysql2/promise");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res
+      .status(400)
+      .json({ error: "Username or Password fields cannot be empty!" });
+    return;
+  }
+
+  try {
+    const existingUser = await checkRecordExists("users", "username", username);
+
+    if (existingUser) {
+      if (!existingUser.password) {
+        res
+        .status(401)
+        .redirect('api/failedLogin');
+        return;
+      }
+
+      const passwordMatch = await bcrypt.compare(
+        password,
+        existingUser.password
+      );
+
+      if (passwordMatch) {
+        res
+        .status(200)
+        .redirect(`/api/dashboard?username=${encodeURIComponent(username)}`);
+      } else {
+        res
+        .status(401)
+        .redirect('api/failedLogin');
+      }
+    } else {
+      res
+      .status(401)
+      .redirect('api/failedLogin');
+    }
+  } catch (error) {
+    res
+    .status(500)
+    .redirect('api/failedLogin');
+  }
+};
+
+module.exports = {
+  login,
+};
