7f4e3f4c4777200a665beee716bbd2ae37fa0780
Charalampos Papadopoulos <harris@easy.box>
2024-12-16 19:42:40 +0200

    added access levels

--- login/authControllers.js
+++ login/authControllers.js
@@ -8,9 +8,7 @@ const login = async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    res
-      .status(400)
-      .json({ error: "Username or Password fields cannot be empty!" });
+    res.status(400).json({ error: "Username or Password fields cannot be empty!" });
     return;
   }
 
@@ -19,35 +17,36 @@ const login = async (req, res) => {
 
     if (existingUser) {
       if (!existingUser.password) {
-        res
-        .status(401)
-        .redirect('api/failedLogin');
+        res.status(401).redirect('api/failedLogin');
         return;
       }
 
-      const passwordMatch = await bcrypt.compare(
-        password,
-        existingUser.password
-      );
+      const passwordMatch = await bcrypt.compare(password, existingUser.password);
 
       if (passwordMatch) {
-        res
-        .status(200)
-        .redirect(`/api/dashboard?username=${encodeURIComponent(username)}`);
+        const token = jwt.sign(
+          { username: existingUser.username, access_level: existingUser.access_level },
+          process.env.JWT_SECRET,
+          { expiresIn: "1h" }
+        );
+
+        res.cookie("auth_token", token, { httpOnly: true }).status(200);
+
+        // Check the access level and redirect accordingly
+        if (existingUser.access_level === 'admin') {
+          res.redirect('/api/admin');
+        } else {
+          res.redirect(`/api/dashboard?username=${encodeURIComponent(username)}`);
+        }
       } else {
-        res
-        .status(401)
-        .redirect('api/failedLogin');
+        res.status(401).redirect('api/failedLogin');
       }
     } else {
-      res
-      .status(401)
-      .redirect('api/failedLogin');
+      res.status(401).redirect('api/failedLogin');
     }
   } catch (error) {
-    res
-    .status(500)
-    .redirect('api/failedLogin');
+    console.log(req.body);
+    res.status(500).redirect('api/failedLogin');
   }
 };
 
